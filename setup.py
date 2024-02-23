eecd5bf66834cbb9e4dd444f46d09a8eb924ffb9
cvijay4u <cvijay4u@gmail.com>
2024-02-23 17:46:47 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Vijay",
+    author_email="cvijay4u@gmail.com",
+    packages=find_packages()
+)
